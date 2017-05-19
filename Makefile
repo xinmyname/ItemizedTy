7a9e6eaa226396c68d293d344282862de8e301d6
Andy Sherwood <sherwooda@cleanwaterservices.org>
2017-05-19 11:46:51 -0700

    Let's start again again again

--- Makefile
+++
@@ -1,9 +0,0 @@
-itemize:
-	yarn
-	./node_modules/.bin/webpack
-	ex -sc '1i|#! /usr/bin/env node' -cx ./bin/itemize
-	chmod +x ./bin/itemize
-
-clean:
-	rm -rf ./bin
-	rm -rf ./node_modules
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,13 @@
-console.log("hello world!")
+import app = require('commander');
+
+app.version('0.0.1')
+    .arguments('<count>')
+    .action(function(count) {
+        console.log("%d items.", count);
+    });
+
+if (process.argv.length == 2) {
+    process.argv.push("1");
+}
+
+app.parse(process.argv);
